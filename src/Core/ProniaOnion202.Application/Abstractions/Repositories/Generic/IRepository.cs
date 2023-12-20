2110570bd4e4d0fec506c727734aaab46587b3df
ibrahimyusifli <tu7MGUER4@code.edu.az>
2023-12-20 10:55:14 +0400

    Onion start

---
+++ src/Core/ProniaOnion202.Application/Abstractions/Repositories/Generic/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using ProniaOnion202.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProniaOnion202.Application.Abstractions.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity, new()
+    {
+        IQueryable<T> GetAll(Expression<Func<T, bool>>? expression = null, Expression<Func<T, object>>? orderExpression = null, bool isDescending = false, bool isTracking = false, params string[] includes);
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void UpdateAsync(T entity);
+        void Delete(T entity);
+        Task SaveChangeAsync();
+        void Update(T existed);
+    }
+}
